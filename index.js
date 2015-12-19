896d3768dddbd2c4e6b6c1c6267c0136bf37f1cd
Eugene Sharygin <eush77@gmail.com>
2015-12-19 10:05:46 +0300

    add debug logging

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 var parseSelector = require('./lib/selector')(),
     select = require('./lib/select');
 
+var debug = require('debug')('unist-util-select');
+
 
 module.exports = function (ast, selector) {
+  debug('Selector: %j', selector);
   selector = parseSelector(selector);
+  debug('AST: %s',
+        JSON.stringify(selector, null, 2).replace(/(^|\n)/g, '\n    '));
   return selector ? select[selector.type](selector, ast) : [];
 };
