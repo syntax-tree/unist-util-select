51dfc483cc463c641818502f3c18cd80e5025fd7
Titus Wormer <tituswormer@gmail.com>
2019-05-23 08:48:20 +0200

    Update dev-dependencies

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
 /* eslint-disable import/no-unassigned-import */
-
 require('./matches')
 require('./select')
 require('./select-all')
 require('./all')
+/* eslint-enable import/no-unassigned-import */
