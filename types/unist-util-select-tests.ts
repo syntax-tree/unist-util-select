3100da1788143eff0272510aa38133be9b9f1a38
Christian Murphy <christian.murphy.42@gmail.com>
2019-10-28 01:02:19 -0700

    Add types
    Closes GH-7.
    
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

---
+++ types/unist-util-select-tests.ts
@@ -0,0 +1,13 @@
+import {matches, select, selectAll} from 'unist-util-select'
+
+matches() // $ExpectError
+matches('*') // $ExpectError
+matches('*', {type: 'root'}) // $ExpectType boolean
+
+select() // $ExpectError
+select('*') // $ExpectError
+select('*', {type: 'root'}) // $ExpectType Node | null
+
+selectAll() // $ExpectError
+selectAll('*') // $ExpectError
+selectAll('*', {type: 'root'}) // $ExpectType Node[]
